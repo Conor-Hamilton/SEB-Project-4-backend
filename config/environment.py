984b23321292ae56d02a1e179ad1ceed3229a948
Conor <conorhamilton@icloud.com>
2024-04-23 13:40:57 +0100

    re-deploy changes

--- config/environment.py
+++ config/environment.py
@@ -1,11 +1,9 @@
 import os
 
-db_URI = "postgresql://postgres:python123@localhost:5432/mma_db"
-SECRET = "tigersparklinglanternstars"
+db_URI = os.getenv(
+    "DATABASE_URL", "postgresql://postgres:python123@localhost:5432/mma_db"
+)
+SECRET = os.getenv("SECRET", "tigersparklinglanternstars")
 
-
-# db_URI = os.getenv("DATABASE_URL")
-# SECRET = os.getenv("SECRET")
-
-# if db_URI.startswith("postgres://"):
-#     db_URI = db_URI.replace("postgres://", "postgresql://", 1)
+if db_URI.startswith("postgres://"):
+    db_URI = db_URI.replace("postgres://", "postgresql://", 1)
